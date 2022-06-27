fc025ab86be45d6fc181d9c5a80978193b728ebb
Altiuss <altiuss@inbox.lv>
2022-06-27 09:16:06 +0200

    add second level

---
+++ src/SecondLevel.java
@@ -0,0 +1,9 @@
+//Write method that checks whether a given string is a palindrome.
+// A palindrome is a phrase that reads the same from the front and the back.
+
+
+public class SecondLevel {
+    public static void main(String[] args) {
+
+    }
+}
